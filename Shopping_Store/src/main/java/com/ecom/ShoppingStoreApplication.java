7a1cd1d1f5afe1e1719a2b9aab18bb3c034c8802
Santosh7972 <malharidoiphode68@gmail.com>
2025-01-01 22:15:26 +0530

    Initial Commit

---
+++ Shopping_Store/src/main/java/com/ecom/ShoppingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingStoreApplication.class, args);
+	}
+
+}
